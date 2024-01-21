a6b8ec443e5410b2c69047338955c1bef67458ff
Naarani <n3ra96@gmail.com>
2024-01-21 22:54:59 +0200

    DONE!

--- mongoose-expenses-project/dist/ExpenseManager.js
+++ mongoose-expenses-project/dist/ExpenseManager.js
@@ -3,15 +3,32 @@
 class ExpenseManager{
 
     constructor(){
-        
+        expensesArr = [] 
     }
 
-    addExpense (){
-
+    addExpense (data){
+       return $.post('/expense', data ,function (response) {
+        expensesArr.push(response)
+        console.log("POST complete")
+    })
     }
 
     getExpenses(){
+        return $.get('/expenses' , function (response){
+            expensesArr.push(response) 
+        })
+    }
 
+    getExpensesByDates(dates){
+        return $.get('/expenses/?'+dates)
+    }
+
+    getExpensesByCategory(category){
+        return $.get('/expenses/'+category)
+    }
+
+    getTotalExpensesByCategory(category){
+        return $.get('/expenses/'+category+'/?total=true')
     }
 
 }
